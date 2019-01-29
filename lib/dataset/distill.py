f5a108e49d00de5ffd360485f39412424344a8b7
Giulio Zhou <giuliozhou8@gmail.com>
2019-01-28 21:18:30 -0800

    Code to read/create annotations JSON for distill dataset

--- lib/dataset/distill.py
+++ lib/dataset/distill.py
@@ -1,5 +1,7 @@
+import cPickle as pickle
 import glob
 import numpy as np
+import os
 import pandas as pd
 import skimage.io as skio
 
@@ -9,6 +11,7 @@ import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
 
+from lib.utils.pycocotools.coco import COCO
 from lib.utils.pycocotools.cocoeval import COCOeval
 
 class DistillDetection(data.Dataset):
@@ -44,6 +47,9 @@ class DistillDetection(data.Dataset):
         self._class_to_ind = dict(zip(self._classes, range(self.num_classes)))
         self._class_to_coco_cat_id = self._class_to_ind
 
+        annofile = self._get_ann_file(label_path)
+        self._COCO = COCO(annofile)
+
     def __getitem__(self, index):
         img = skio.imread(self.data[index])
         height, width = img.shape[:2]
@@ -61,8 +67,58 @@ class DistillDetection(data.Dataset):
     def __len__(self):
         return len(self.data)
 
+    def pull_image(self, index):
+        img = skio.imread(self.data[index])
+        return img
 
+    def pull_anno(self, index):
+        img = skio.imread(self.data[index])
+        height, width = img.shape[:2]
+        target = np.array(self.labels[index]).reshape(-1, 5)
+        target[:, :4] *= [width, height, width, height]
+        return [index, [(1, t) for t in target]]
 
+    def _get_ann_file(self, label_path, default_path='distill_anno.json'):
+        if os.path.exists(default_path):
+            return default_path
+        df = pd.read_csv(label_path)
+        filenames = sorted(np.unique(df['filename']))
+        i = 0
+        json_file = {}
+        json_file['categories'] = [dict(id=1, name='obj', supercategory='obj')]
+        json_file['images'], json_file['annotations'] = [], []
+        for filename in filenames:
+            print(filename)
+            img_paths = sorted(glob.glob(filename + '/*'))
+            height, width = skio.imread(img_paths[0]).shape[:2]
+            for frame_no in range(len(img_paths)):
+                frame_df = df[(df['filename'] == filename) &
+                              (df['frame_no'] == frame_no)]
+                json_file['images'].extend(
+                  [{'id': i,
+                    'width': width,
+                    'height': height,
+                    'file_name': img_paths[frame_no]}])
+                if len(frame_df) == 0:
+                    i += 1
+                    continue
+                dets = np.array(frame_df[['xmin', 'ymin', 'xmax', 'ymax']])
+                dets *= [width, height, width, height]
+                xs = dets[:, 0]
+                ys = dets[:, 1]
+                ws = dets[:, 2] - xs # + 1
+                hs = dets[:, 3] - ys # + 1
+                xs, ys, ws, hs = map(lambda x: map(float, x), [xs, ys, ws, hs])
+                json_file['annotations'].extend(
+                  [{'id': i, 'image_id' : i,
+                    'category_id' : 1, # only one category
+                    'bbox' : [xs[k], ys[k], ws[k], hs[k]],
+                    'area': ws[k]*hs[k],
+                    'iscrowd': 0} for k in range(dets.shape[0])])
+                i += 1
+        with open(default_path, 'w') as outfile:
+            json.dump(json_file, outfile)
+            return default_path
 
     def _print_detection_eval_metrics(self, coco_eval):
         IoU_lo_thresh = 0.5
@@ -136,10 +192,31 @@ class DistillDetection(data.Dataset):
         with open(res_file, 'w') as fid:
             json.dump(results, fid)
 
+    def _coco_results_one_category(self, boxes, cat_id):
+        results = []
+        for im_ind in range(len(self.data)):
+            dets = boxes[im_ind]
+            if dets == []:
+                continue
+            dets = np.float32(dets)
+            scores = dets[:, -1]
+            xs = dets[:, 0]
+            ys = dets[:, 1]
+            ws = dets[:, 2] - xs # + 1
+            hs = dets[:, 3] - ys # + 1
+            xs, ys, ws, hs, scores = map(lambda x: map(float, x),
+                                         [xs, ys, ws, hs, scores])
+            results.extend(
+                [{'image_id' : im_ind,
+                  'category_id' : cat_id,
+                  'bbox' : [xs[k], ys[k], ws[k], hs[k]],
+                  'score' : scores[k]} for k in range(dets.shape[0])])
+        return results
+
     def evaluate_detections(self, all_boxes, output_dir):
         res_file = os.path.join(output_dir, ('detection_results'))
         res_file += '.json'
         self._write_coco_results_file(all_boxes, res_file)
         # Only do evaluation on non-test sets
-        if self.coco_name.find('test') == -1:
-            self._do_detection_eval(res_file, output_dir)
+        # if self.coco_name.find('test') == -1:
+        self._do_detection_eval(res_file, output_dir)
